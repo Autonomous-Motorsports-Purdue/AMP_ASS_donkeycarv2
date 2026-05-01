c00a92f6206cb27f0bb2910f81d564cfcf23ad15
Manav Gagvani <mgagvani@outlook.com>
2026-04-30 22:21:23 -0400

    cross track error

---
+++ cte_runner.py
@@ -0,0 +1,56 @@
+import donkeycar as dk
+
+from parts.uart_backup import UART_backup_driver
+from parts.gps import GPS
+from parts.gps_to_xy import GPS_to_xy
+from parts.health_check import HealthCheck
+from parts.cte_controller import CTEController
+
+import numpy as np
+
+'''
+checklist if not working
+
+1. verify IMU, GPS, Main PCB UART are plugged in.
+2. verify ports for each. should be /dev/tty* or /dev/usb*. unplug to test
+
+run this to download the offline map of WL: 
+```
+python 
+```
+'''
+import argparse
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", help="gps waypoint csv file name")
+    args = parser.parse_args()
+
+    data = np.genfromtxt(args.file_name, delimiter=',', skip_header=1, dtype=float, encoding='utf-8')
+    ref_lat0 = data[0,0]
+    ref_lon0 = data[0,1]
+
+    V = dk.vehicle.Vehicle()
+
+    # Heart beat
+    heartbeat= HealthCheck("192.168.12.25", 6000) # just returns true rn
+    V.add(heartbeat, inputs=[], outputs=["safety/heartbeat"])
+
+    # # UART driver
+    uart = UART_backup_driver("/dev/ttyACM0")
+    V.add(uart, inputs=["controls/throttle", "controls/steering", "safety/heartbeat"], outputs=[], threaded=False)
+
+    gps = GPS('/dev/ttyACM2')
+    V.add(gps, inputs=[], outputs=['lat_raw', 'lon_raw', 'alt', 'fix', 'corr_age', 'hdop', 'sat_count', 'gps_heading'], threaded=True)
+
+    # GPS to XY
+    gps_to_xy = GPS_to_xy(ref_lat_deg=ref_lat0, ref_lon_deg=ref_lon0) # first point as origin
+    V.add(gps_to_xy, inputs=["lat_raw", "lon_raw"], outputs=["x", "y", "gps_yaw"], threaded=False)
+
+    # MPC Controller
+    csv_xy_path = args.file_name.split('.')[0] + "_xy" + ".csv"
+    throttle = 1000
+    kp, ki, kd = 0.2, 0.0, 0.1
+    controller = CTEController(path_csv=csv_xy_path, throttle=throttle, kp=kp, ki=ki, kd=kd)
+    V.add(controller, inputs=["x", "y", "gps_yaw"], outputs=["controls/throttle", "controls/steering"], threaded=False)
+
+    V.start(rate_hz=100, max_loop_count=None)
---
+++ parts/cte_controller.py
@@ -0,0 +1,229 @@
+import numpy as np
+import time
+from donkeycar.la import Line3D, Vec3
+from donkeycar.utils import dist
+import logging
+
+# Helper Classes
+
+# donkeycar/parts/transform.py
+class PIDController:
+    """ Performs a PID computation and returns a control value.
+        This is based on the elapsed time (dt) and the current value
+        of the process variable
+        (i.e. the thing we're measuring and trying to change).
+        https://github.com/chrisspen/pid_controller/blob/master/pid_controller/pid.py
+    """
+
+    def __init__(self, p=0, i=0, d=0, debug=False):
+
+        # initialize gains
+        self.Kp = p
+        self.Ki = i
+        self.Kd = d
+
+        # The value the controller is trying to get the system to achieve.
+        self.target = 0
+
+        # initialize delta t variables
+        self.prev_tm = time.time()
+        self.prev_feedback = 0
+        self.error = None
+
+        # initialize the output
+        self.alpha = 0
+
+        # debug flag (set to True for console output)
+        self.debug = debug
+
+    def run(self, target_value, feedback):
+        curr_tm = time.time()
+
+        self.target = target_value
+        error = self.error = self.target - feedback
+
+        # Calculate time differential.
+        dt = curr_tm - self.prev_tm
+
+        # Initialize output variable.
+        curr_alpha = 0
+
+        # Add proportional component.
+        curr_alpha += self.Kp * error
+
+        # Add integral component.
+        curr_alpha += self.Ki * (error * dt)
+
+        # Add differential component (avoiding divide-by-zero).
+        if dt > 0:
+            curr_alpha += self.Kd * ((feedback - self.prev_feedback) / float(dt))
+
+        # Maintain memory for next loop.
+        self.prev_tm = curr_tm
+        self.prev_feedback = feedback
+
+        # Update the output
+        self.alpha = curr_alpha
+
+        if (self.debug):
+            print('PID target value:', round(target_value, 4))
+            print('PID feedback value:', round(feedback, 4))
+            print('PID output:', round(curr_alpha, 4))
+
+        return curr_alpha
+
+# donkeycar/parts/path.py
+class CTE(object):
+
+    def __init__(self, look_ahead=1, look_behind=1, num_pts=None) -> None:
+        self.num_pts = num_pts
+        self.look_ahead = look_ahead
+        self.look_behind = look_behind
+
+    #
+    # Find the index of the path element with minimal distance to (x,y).
+    # This prefers the first element with the minimum distance if there
+    # are more then one.
+    #
+    def nearest_pt(self, path, x, y, from_pt=0, num_pts=None):
+        from_pt = from_pt if from_pt is not None else 0
+        num_pts = num_pts if num_pts is not None else len(path)
+        num_pts = min(num_pts, len(path))
+        if num_pts < 0:
+            logging.error("num_pts must not be negative.")
+            return None, None, None
+
+        min_pt = None
+        min_dist = None
+        min_index = None
+        for j in range(num_pts):
+            i = (j + from_pt) % len(path)
+            p = path[i]
+            d = dist(p[0], p[1], x, y)
+            if min_dist is None or d < min_dist:
+                min_pt = p
+                min_dist = d
+                min_index = i
+        return min_pt, min_index, min_dist
+
+
+    # TODO: update so that we look for nearest two points starting from a given point
+    #       and up to a given number of points.  This will speed things up
+    #       but more importantly it can be used to handle crossing paths.
+    def nearest_two_pts(self, path, x, y):
+        if path is None or len(path) < 2:
+            logging.error("path is none; cannot calculate nearest points")
+            return None, None
+
+        distances = []
+        for iP, p in enumerate(path):
+            d = dist(p[0], p[1], x, y)
+            distances.append((d, iP, p))
+        distances.sort(key=lambda elem : elem[0])
+
+        # get the prior point as start of segment
+        iA = (distances[0][1] - 1) % len(path)
+        a = path[iA]
+
+        # get the next point in the path as the end of the segment
+        iB = (iA + 2) % len(path)
+        b = path[iB]
+        
+        return a, b
+
+    def nearest_waypoints(self, path, x, y, look_ahead=1, look_behind=1, from_pt=0, num_pts=None):
+        """
+        Get the path elements around the closest element to the given (x,y)
+        :param path: list of (x,y) points
+        :param x: horizontal coordinate of point to check
+        :param y: vertical coordinate of point to check
+        :param from_pt: index start start search within path
+        :param num_pts: maximum number of points to search in path
+        :param look_ahead: number waypoints to include ahead of nearest point.
+        :param look_behind: number of waypoints to include behind nearest point.
+        :return: index of first point, nearest point and last point in nearest path segments
+        """
+        if path is None or len(path) < 2:
+            logging.error("path is none; cannot calculate nearest points")
+            return None, None
+
+        if look_ahead < 0:
+            logging.error("look_ahead must be a non-negative number")
+            return None, None
+        if look_behind < 0:
+            logging.error("look_behind must be a non-negative number")
+            return None, None
+        if (look_ahead + look_behind) > len(path):
+            logging.error("the path is not long enough to supply the waypoints")
+            return None, None
+
+        _pt, i, _distance = self.nearest_pt(path, x, y, from_pt, num_pts)
+
+        # get  start of segment
+        a = (i + len(path) - look_behind) % len(path)
+
+        # get the end of the segment
+        b = (i + look_ahead) % len(path)
+
+        return a, i, b
+
+    def nearest_track(self, path, x, y, look_ahead=1, look_behind=1, from_pt=0, num_pts=None):
+        """
+        Get the line segment around the closest point to the given (x,y)
+        :param path: list of (x,y) points
+        :param x: horizontal coordinate of point to check
+        :param y: vertical coordinate of point to check
+        :param from_pt: index start start search within path
+        :param num_pts: maximum number of points to search in path
+        :param look_ahead: number waypoints to include ahead of nearest point.
+        :param look_behind: number of waypoints to include behind nearest point.
+        :return: start and end points of the nearest track and index of nearest point
+        """
+
+        a, i, b = self.nearest_waypoints(path, x, y, look_ahead, look_behind, from_pt, num_pts)
+
+        return (path[a], path[b], i) if a is not None and b is not None else (None, None, None)
+
+    def run(self, path, x, y, from_pt=None):
+        """
+        Run cross track error algorithm
+        :return: cross-track-error and index of nearest point on the path
+        """
+        cte = 0.
+        i = from_pt
+
+        a, b, i = self.nearest_track(path, x, y, 
+                                     look_ahead=self.look_ahead, look_behind=self.look_behind, 
+                                     from_pt=from_pt, num_pts=self.num_pts)
+        
+        if a and b:
+            logging.info(f"nearest: ({a[0]}, {a[1]}) to ({x}, {y})")
+            a_v = Vec3(a[0], 0., a[1])
+            b_v = Vec3(b[0], 0., b[1])
+            p_v = Vec3(x, 0., y)
+            line = Line3D(a_v, b_v)
+            err = line.vector_to(p_v)
+            sign = 1.0
+            cp = line.dir.cross(err.normalized())
+            if cp.y > 0.0 :
+                sign = -1.0
+            cte = err.mag() * sign            
+        else:
+            logging.info(f"no nearest point to ({x},{y}))")
+        return cte, i
+    
+class CTEController:
+    def __init__(self, path_csv, throttle=1000, kp=0.5, ki=0.0, kd=0.0):
+        self.cte = CTE(look_ahead=1, look_behind=1)
+        self.pid = PIDController(p=kp, i=ki, d=kd, debug=False)
+        self.path = np.genfromtxt(path_csv, delimiter=',', skip_header=1, dtype=float, encoding='utf-8')
+        self.throttle = throttle # TODO: change to be adaptive based on selected point in path
+
+    def run(self, x, y, yaw):
+        cte, idx = self.cte.run(self.path, x, y)
+        steer = self.pid.run(0.0, cte) # we desire 0 cte
+
+        if self.pid.debug:
+            print('CTE:', round(cte, 4))
+        
+        return self.throttle, steer
