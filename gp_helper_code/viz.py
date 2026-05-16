6f610f55fa8716f88081b236f07ad8d53439da5e
Nrxszv0 <michael.crothersmc@gmail.com>
2026-05-16 15:47:02 -0400

    edit cte_runner

--- gp_helper_code/viz.py
+++ gp_helper_code/viz.py
@@ -29,7 +29,6 @@ fig, ax = plt.subplots(figsize=(8, 8))
 ax.imshow(map_img, extent=BBOX, aspect='equal')
 
 # Initialize plot elements
-trajectory_line, = ax.plot([], [], 'b-', linewidth=2, label='Trajectory') # Blue line
 robot_marker, = ax.plot([], [], 'ro', markersize=6, label='Current Pos') # Red dot
 # Quiver for heading (arrow). Initialized at 0,0
 heading_arrow = ax.quiver(0, 0, 0, 0, color='yellow', scale=15, pivot='tail', zorder=5)
@@ -38,9 +37,24 @@ heading_arrow = ax.quiver(0, 0, 0, 0, color='yellow', scale=15, pivot='tail', zo
 steer_arrow = ax.quiver(0, 0, 0, 0, color='red', scale=20, pivot='tail', zorder=5)
 
 # file_path = "../gps_paths/gp_track/gp_raceline_gps.csv"
-file_path = "../gps_paths/gp_track/gp_line.csv"
-reference_path_df = pd.read_csv(file_path)
+# file_path = "../gps_paths/gps_raceline_edited_v2.csv"
+# file_path = "../gps_paths/gp_midline_dense2.csv"
+file_path_1 = "../gps_paths/planned_aligned.csv"
+
+
+ref_gp_path = "../gps_paths/2026-05-01-14-59-55_gps_full.csv"
+reference_path_df = pd.read_csv(ref_gp_path)
 ref_path = ax.plot(reference_path_df["longitude"],reference_path_df["latitude"], label='Reference')
+
+# align_path_file = "../gps_paths/planned_aligned.csv"
+# align_path_df = pd.read_csv(align_path_file)
+# align_path = ax.plot(align_path_df["longitude"],align_path_df["latitude"], label='Aligned')
+
+follow_path_file = "../gps_paths/gp_raceline_gps.csv" 
+follow_path_df = pd.read_csv(follow_path_file)
+follow_path = ax.plot(follow_path_df["longitude"], follow_path_df["latitude"],label="Follow")
+
+trajectory_line, = ax.plot([], [], 'b-', linewidth=2, label='Trajectory') # Blue line
 
 ax.set_xlim(BBOX[0], BBOX[1])
 ax.set_ylim(BBOX[2], BBOX[3])
@@ -48,11 +62,15 @@ ax.set_title("Live Robot Telemetry (Offline Map)")
 ax.set_xlabel("Longitude")
 ax.set_ylabel("Latitude")
 ax.legend()
+# ax.show()
+# while True:
+#     pass
 
 # Data history arrays
 history_lat = []
 history_lon = []
 
+# ssh -L 5005:localhost:5005 amp@192.168.1.50
 # --- NETWORK SETUP ---
 PORT = 5005
 buffer = ""
@@ -72,15 +90,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             buffer += chunk
             
             # Process complete JSON messages separated by newline
+            lines = buffer.split('\n')
             while '\n' in buffer:
-                line, buffer = buffer.split('\n', 1)
-                data = json.loads(line)
+                # line, buffer = buffer.split('\n', 1)
+                latest_line = lines[-2] 
+                buffer = lines[-1] # Keep the remainder for next time
+                # data = json.loads(line)
+                data = json.loads(latest_line)
                 print(data)
                 
+                has_null = any(v is None for v in data.values())
+                if data['lat'] != None and data['heading'] == None:
+                    print(f"override heading null")
+                    has_null = False
+                if has_null:
+                    print("null found, continuing")
+                    continue
                 lat = data['lat']
                 lon = data['lon']
                 # heading is in radians?
-                heading_deg = np.rad2deg(data['heading'])
+                if data['heading'] == None:
+                    heading_deg = 0
+                else:
+                    heading_deg = np.rad2deg(data['heading'])
 
                 if 'steer' in data:
                     steer_val = data['steer'] * max_steer
